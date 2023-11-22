b154c74e2f4fa6cb7761dffe3e42bc4ff1f5062e
wuzoo <wndyd9911@gmail.com>
2023-11-22 13:44:26 +0900

    feat: firebase의 유저 validation =>  auth.authStateReady()

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Layout from "./components/Layout";
 import Home from "./routes/Home";
 import Profile from "./routes/Profile";
 import Login from "./routes/Login";
 import CreateAccount from "./routes/Create-account";
 import Globalstyle from "./GlobalStyle";
+import LoadingScreen from "./components/Loading-screen";
+import auth from "./components/firebase";
+import { styled } from "styled-components";
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
 
 const router = createBrowserRouter([
   {
@@ -20,11 +30,23 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const init = async () => {
+    // wait for firebase about user authentication
+    await auth.authStateReady(); // auth 상태가 준비될때까지 기다리고, 최초 인증 상태가 완료될 때 실행되는 Promise를 리턴
+    // firebase가 쿠키와 토큰을 읽을 시간
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    init();
+  }, []);
+
   return (
-    <>
+    <Wrapper>
       <Globalstyle />
-      <RouterProvider router={router} />
-    </>
+      {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
+    </Wrapper>
   );
 }
 
