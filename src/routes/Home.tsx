cadf7655bf019aad256abac9206536130476affb
wuzoo <wndyd9911@gmail.com>
2023-11-22 14:19:09 +0900

    feat: auth.signOut() 으로 로그인된 유저 로그아웃

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,3 +1,12 @@
+import auth from "../firebase";
+
 export default function Home() {
-  return <h1>Home !</h1>;
+  const logout = () => {
+    auth.signOut();
+  };
+  return (
+    <h1>
+      <button onClick={logout}>Log Out</button>
+    </h1>
+  );
 }
