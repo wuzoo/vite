f3480a2bba2d412ff542df270ffc341b92dc92f7
wuzoo <wndyd9911@gmail.com>
2023-11-22 13:46:21 +0900

    feat: 기본적인 Join 페이지 JSX 코드 설계

--- src/routes/Create-account.tsx
+++ src/routes/Create-account.tsx
@@ -1,3 +1,101 @@
+import { styled } from "styled-components";
+import { useState } from "react";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import auth from "../components/firebase";
+import { useNavigate } from "react-router-dom";
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 420px;
+  padding: 50px 0px;
+`;
+const Form = styled.form`
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 100%;
+`;
+const Input = styled.input`
+  padding: 10px 20px;
+  border-radius: 50px;
+  border: none;
+  width: 100%;
+  font-size: 16px;
+  &[type="submit"] {
+    cursor: pointer;
+    &: hover {
+      opacity: 0.8;
+    }
+  }
+`;
+const Title = styled.h1`
+  font-size: 42px;
+`;
+const Error = styled.span`
+  font-weight: 600;
+  color: tomato;
+`;
+
 export default function CreateAccount() {
-  return <h1>Create Account !</h1>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "name") {
+      setName(value);
+    } else if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  return (
+    <Wrapper>
+      <Title>Join X</Title>
+      <Form onSubmit={onSubmit}>
+        <Input
+          name="name"
+          value={name}
+          placeholder="Name"
+          type="text"
+          required
+          onChange={onChange}
+        />
+        <Input
+          name="email"
+          value={email}
+          placeholder="Email"
+          type="email"
+          required
+          onChange={onChange}
+        />
+        <Input
+          name="password"
+          value={password}
+          placeholder="Password"
+          type="password"
+          required
+          onChange={onChange}
+        />
+        <Input
+          type="submit"
+          value={isLoading ? "Loading..." : "Create Account"}
+        />
+      </Form>
+      {error !== "" ? <Error>{error}</Error> : ""}
+    </Wrapper>
+  );
 }
