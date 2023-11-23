bed5b33decde538c5b63d2aae4ada7daac48bfd4
wuzoo <wndyd9911@gmail.com>
2023-11-23 15:24:50 +0900

    feat: Login 페이지 구조 설계

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,3 +1,90 @@
+import { styled } from "styled-components";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { FirebaseError } from "firebase/app";
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 420px;
+  padding: 200px 0px;
+`;
+const Form = styled.form`
+  margin-top: 50px;
+  margin-bottom: 10px;
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 100%;
+`;
+const Input = styled.input`
+  padding: 10px 20px;
+  border-radius: 50px;
+  border: none;
+  width: 100%;
+  font-size: 16px;
+  &[type="submit"] {
+    cursor: pointer;
+    &: hover {
+      opacity: 0.8;
+    }
+  }
+`;
+const Title = styled.h1`
+  font-size: 42px;
+`;
+const Error = styled.span`
+  font-weight: 600;
+  color: tomato;
+`;
+
 export default function Login() {
-  return <h1>Login !</h1>;
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  return (
+    <Wrapper>
+      <Title>Login into X</Title>
+      <Form onSubmit={onSubmit}>
+        <Input
+          name="email"
+          value={email}
+          placeholder="Email"
+          type="email"
+          required
+          onChange={onChange}
+        />
+        <Input
+          name="password"
+          value={password}
+          placeholder="Password"
+          type="password"
+          required
+          onChange={onChange}
+        />
+        <Input
+          type="submit"
+          value={isLoading ? "Loading..." : "Create Account"}
+        />
+      </Form>
+      {error !== "" ? <Error>{error.split("Firebase:")}</Error> : ""}
+    </Wrapper>
+  );
 }
