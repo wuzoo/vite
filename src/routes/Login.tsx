3e6ddaf2da9ddff2e6da94bc57f99f99044b6e83
wuzoo <wndyd9911@gmail.com>
2023-11-23 15:48:53 +0900

    refactor: auth 관련 페이지 컴포넌트 분리

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,53 +1,16 @@
-import { styled } from "styled-components";
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { FirebaseError } from "firebase/app";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import auth from "../firebase";
-
-const Wrapper = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 420px;
-  padding: 200px 0px;
-`;
-const Form = styled.form`
-  margin-top: 50px;
-  margin-bottom: 10px;
-  margin-top: 50px;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  width: 100%;
-`;
-const Input = styled.input`
-  padding: 10px 20px;
-  border-radius: 50px;
-  border: none;
-  width: 100%;
-  font-size: 16px;
-  &[type="submit"] {
-    cursor: pointer;
-    &: hover {
-      opacity: 0.8;
-    }
-  }
-`;
-const Title = styled.h1`
-  font-size: 42px;
-`;
-const Error = styled.span`
-  font-weight: 600;
-  color: tomato;
-`;
-const Switcher = styled.div`
-  margin-top: 20px;
-  a {
-    color: #1d9bf0;
-  }
-`;
+import {
+  Error,
+  Form,
+  Input,
+  Switcher,
+  Title,
+  Wrapper,
+} from "../components/auth-component";
 
 export default function Login() {
   const [isLoading, setIsLoading] = useState(false);
