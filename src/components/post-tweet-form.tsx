e92592b8cf7688c54415c39277f363eaeeee29bb
wuzoo <wndyd9911@gmail.com>
2023-12-11 03:52:25 +0900

    feat: post tweet form 설계 및 스타일

---
+++ src/components/post-tweet-form.tsx
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const TextArea = styled.textarea`
+  border: 2px solid white;
+  padding: 20px;
+  border-radius: 20px;
+  font-size: 16px;
+  color: white;
+  background-color: black;
+  width: 100%;
+  height: 150px;
+  resize: none;
+  &::placeholder {
+    font-size: 16px;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI",
+      Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
+      sans-serif;
+  }
+  &:focus {
+    outline: none;
+    border-color: #1d9bf0;
+  }
+`;
+
+const AttachFileButton = styled.label`
+  padding: 10px 0px;
+  color: #1d9bf0;
+  text-align: center;
+  border-radius: 20px;
+  border: 1px solid #1d9bf0;
+  font-size: 14px;
+  font-weight: 600;
+  cursor: pointer;
+`;
+
+const AttachFileInput = styled.input`
+  display: none;
+`;
+
+const SubmitBtn = styled.button`
+  background-color: #1d9bf0;
+  color: white;
+  border: none;
+  padding: 10px 0px;
+  border-radius: 20px;
+  font-size: 16px;
+  cursor: pointer;
+  &:hover,
+  &:active {
+    opacity: 0.8;
+  }
+`;
+
+export default function PostTweetForm() {
+  return (
+    <Form>
+      <TextArea placeholder="What is happening"></TextArea>
+      <AttachFileButton htmlFor="file">Add Photo</AttachFileButton>
+      <AttachFileInput type="file" id="file" accept="image/*" />
+      <SubmitBtn type="submit" value="Post Tweet" />
+    </Form>
+  );
+}
