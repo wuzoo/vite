21d1e7a1d7187112897c1f07fac1b10ede86dc98
wuzoo <wndyd9911@gmail.com>
2023-12-19 15:14:08 +0900

    feat: 모달에서 Edit 하려는 Tweet 접근 위해 Tweet 전역 객체 설정

--- src/atoms/atom.ts
+++ src/atoms/atom.ts
@@ -1,6 +1,23 @@
 import { atom } from "recoil";
 
+interface IEditTweet {
+  userId: string;
+  photo?: string;
+  id: string;
+  tweet: string;
+}
+
 export const EditClicked = atom({
   key: "EditClicked",
   default: false,
+});
+
+export const EditTweet = atom<IEditTweet>({
+  key: "EditTweet",
+  default: {
+    userId: "",
+    photo: "",
+    id: "",
+    tweet: "",
+  },
 });
