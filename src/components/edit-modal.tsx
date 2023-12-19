05d466f5a52afd7defc52dea0f86c4c4a95f8c1b
wuzoo <wndyd9911@gmail.com>
2023-12-19 15:18:01 +0900

    feat: Edit Tweet의 Doc 가져오기 + update

--- src/components/edit-modal.tsx
+++ src/components/edit-modal.tsx
@@ -10,6 +10,8 @@ import {
 } from "./post-tweet-form";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { EditClicked, EditTweet } from "../atoms/atom";
+import auth, { db } from "../firebase";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { useState } from "react";
 
 const Overlay = styled.div`
@@ -36,15 +38,36 @@ export default function EditModal() {
   const [EditisClicked, setEditisClicked] = useRecoilState(EditClicked);
   const tweet = useRecoilValue(EditTweet);
 
+  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTw(e.target.value);
+  };
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // const user = auth.currentUser;
+
+    try {
+      const docRef = doc(db, "tweets", tweet.id);
+      const docSnap = await getDoc(docRef);
+      console.log(docSnap.data());
+
+      await updateDoc(docRef, {
+        tweet: tw,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return parentElement && EditisClicked
     ? ReactDOM.createPortal(
         <Overlay>
-          <EditForm>
+          <EditForm onSubmit={onSubmit}>
             <TextArea
               rows={5}
               maxLength={160}
               placeholder={tweet.tweet}
-              //   onChange={onChange}
+              onChange={onChange}
               value={tw}
             ></TextArea>
             <AttachFileButton htmlFor="file">Edit Photo</AttachFileButton>
